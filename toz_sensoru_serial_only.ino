e4f8a75aba7f91d845ce99e42f3f82b7280bce2c
Onoktake <orcun.k.kucuk@gmail.com>
2020-05-18 19:07:06 +0300

    Update toz_sensoru_serial_only.ino

--- toz_sensoru_serial_only.ino
+++ toz_sensoru_serial_only.ino
@@ -1,9 +1,6 @@
 int veriPini = A0; // Siyah kablonun (sensörün soldan 2. pini) bağlı olduğu pin
 float toz=0; //Gelen veriyi sakladığımız değişken
 int led=2; // Beyaz kablonun (sensörün soldan 3. pini) bağlı olduğu pin
-int delayTime=280; 
-int delayTime2=40;
-float offTime=9680;
 
 void setup(){
 Serial.begin(9600); //Verileri okuyabilmek için seri portu başlatıyoruz.
@@ -12,14 +9,13 @@ pinMode(veriPini, INPUT);
 }
  
 void loop(){
-
 digitalWrite(led,LOW); 
-delayMicroseconds(delayTime);
-dustVal=analogRead(veriPini); 
-delayMicroseconds(delayTime2);
+delayMicroseconds(280);
+toz=analogRead(veriPini); 
+delayMicroseconds(40);
 digitalWrite(led,HIGH); 
-delayMicroseconds(offTime);
+delayMicroseconds(9860);
 delay(1000);
-if (dustVal>36.455)
-Serial.println((float(dustVal/1024)-0.0356)*120000*0.035);
+if (toz>36.455)
+Serial.println((float(toz/1024)-0.0356)*120000*0.035);
 }
